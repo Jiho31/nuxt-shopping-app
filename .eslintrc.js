d7b11b5f44f912c7507587be814190a52feb547e
복지호 <jihobok@smilegate.sg>
2023-04-18 16:26:04 +0900

    eslint, prettier 규칙 적용

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    // vue
+    // 'plugin:vue/vue3-essential', // Lv1
+    // 'plugin:vue/vue3-strongly-recommended', // Lv2
+    // 'plugin:vue/vue3-recommended', // Lv3
+    'plugin:vue/strongly-recommended',
+    // js
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser'
+  },
+  rules: {
+    "vue/multi-word-component-names": 0,
+  }
+}
