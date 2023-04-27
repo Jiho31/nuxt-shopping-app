7fdd06c4175602b1ee4334f0b1519b2a4f03f6b1
복지호 <jihobok@smilegate.sg>
2023-04-27 11:18:52 +0900

    SASS 강의 ~ #6

---
+++ sass/gulpfile.js
@@ -0,0 +1,15 @@
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+
+function buildStyles() {
+  return src("shinobi/**/*.scss")
+    .pipe(sass()) // sass 컴파일러를 통해 scss 파일을 css로 컴파일
+    .pipe(dest("css")); // 컴파일된 css 파일이 출력되는 경로
+}
+
+// sass 파일이 변경되면 자동으로 buildStyle이 실행되면서 css 파일에 내용이 반영됨
+function watchTask() {
+  watch(["shinobi/**/*.scss"], buildStyles);
+}
+
+exports.default = series(buildStyles, watchTask);
